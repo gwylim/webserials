be54e60f6e39cad7fe55ed308ad7a8245ef1cd5a
Gwylim Ashley <gwylim.a@gmail.com>
2017-12-22 13:22:33 -0800

    Implement better command line help

--- src/main.js
+++ src/main.js
@@ -31,11 +31,57 @@ async function scrape(fetch: Serial) {
     epub.end();
 }
 
-// TODO: Use argparse to implement proper argument handling
-if (process.argv[2] === 'fanfiction') {
-    scrape(fanfiction('fanfiction.net', process.argv[3]));
-} else if (process.argv[2] === 'fictionpress') {
-    scrape(fanfiction('fictionpress.com', process.argv[3]));
-} else {
-    scrape(serials[process.argv[2]]);
+function printHelp() {
+    console.error('usage: main.js fictionpress <id>');
+    console.error('       main.js fanfiction <id>');
+    console.error('       main.js <name>');
+    console.error('       main.js --list-serials');
+    console.error('       main.js --help');
+    console.error();
+    console.error('Generate EPUBs for various web serials');
+    console.error();
+    console.error('Arguments');
+    console.error('  name              Name of serial to fetch, see --list-serials');
+    console.error('  id                ID of serial on fictionpress.com or fanfiction.net');
+    console.error('  --help            Display this message and exit');
+    console.error('  --list-serials    Show available serials and exit');
 }
+
+function error(message) {
+    console.error('error: ' + message);
+    console.error();
+    printHelp();
+    process.exit(1);
+}
+
+if (process.argv.length < 3) {
+    error('Not enough arguments');
+}
+if (process.argv[2] === '--help') {
+    printHelp();
+    process.exit(0);
+}
+if (process.argv[2] === '--list-serials') {
+    console.error('Available serials');
+    for (let name in serials) {
+        if (serials.hasOwnProperty(name)) {
+            console.error('  ' + name);
+        }
+    }
+    process.exit(0);
+}
+const fictionpress = process.argv[2] === 'fictionpress' || process.argv[2] === 'fanfiction';
+if (fictionpress && process.argv.length < 4) {
+    error('ID not provided');
+}
+if (fictionpress && isNaN(parseInt(process.argv[3]))) {
+    error('ID must be a number');
+}
+if (fictionpress) {
+    scrape(fanfiction(process.argv[2] === 'fictionpress' ? 'fictionpress.com' : 'fanfiction.net', process.argv[3]));
+    process.exit(0);
+}
+if (!serials[process.argv[2]]) {
+    error('Unrecognized serial ' + process.argv[2]);
+}
+scrape(serials[process.argv[2]]);
